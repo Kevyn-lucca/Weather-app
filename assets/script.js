61501a8374e77ac96f106e184a3a15837ca8b055
kevyn <kevynalternativo@gmail.com>
2024-05-25 21:02:07 -0300

    Task: Implementar os dados da api na pagina

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// http://api.weatherapi.com/v1/forecast.json?key=ece09bec73a74ca391d224224242205&q=brazil&days=6&aqi=no&alerts=no
+/*
+async function GetWeather() {
+  const WeatherResponse = await fetch(
+    "http://api.weatherapi.com/v1/forecast.json?key=ece09bec73a74ca391d224224242205&q=brazil&days=6&aqi=no&alerts=no",
+    { mode: "cors" }
+  );
+  const WeatherData = await WeatherResponse.json();
+  console.log(WeatherData);
+}
+
+GetWeather();
+*/
